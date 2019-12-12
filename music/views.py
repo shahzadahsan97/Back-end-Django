c290507e9e20f27d74ff055a4ce366eb04621303
Shahzad Ahsan <dankara97@gmail.com>
2019-12-12 13:02:30 +0500

    add music app to web url

--- music/views.py
+++ music/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h1>This is the music app home page</h1>")
 
 # Create your views here.
