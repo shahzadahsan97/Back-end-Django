50f02bb250671fc2bd623df6299fa5ac580f77d8
Shahzad Ahsan <dankara97@gmail.com>
2019-12-18 13:40:02 +0500

    make special method to call album_title and artist to database

--- music/models.py
+++ music/models.py
@@ -9,10 +9,14 @@ class Album(models.Model):
     album_logo= models.CharField(max_length=1000)
 
 
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
 class Song(models.Model):
     album= models.ForeignKey(Album , on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
     
 
-    
+
