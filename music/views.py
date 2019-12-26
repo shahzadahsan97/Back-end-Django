acb56c968c66ac5db1ba73fb912f20905c8c680a
Shahzad Ahsan <dankara97@gmail.com>
2019-12-26 16:08:57 +0500

    connecting page to database

--- music/views.py
+++ music/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 
 
 def index(request):
-    return HttpResponse("<h1>This is a music app home page</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url +'">'+ album.album_title + '</a><br>'
+    return HttpResponse(html)
+
 
 def details(requset , album_id):
     return HttpResponse("<h2>Details for album id is:" + str(album_id) + "</h2>")
